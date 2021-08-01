034b612481dccb28aea97a98ad0940427ffec95d
Alfredo-Rojas <alfredot84@gmail.com>
2021-08-01 08:38:09 -0400

    Node-sass and files for homepage added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Homepage from './pages/homepage/Homepage';
 
 function App() {
   return (
     <div>
-      Hello
+      <Homepage />
     </div>
   );
 }
